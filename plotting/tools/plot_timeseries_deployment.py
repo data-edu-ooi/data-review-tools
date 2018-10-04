74f55df87b2088dd1d14fa4a18c3bb6cf56aec54
lgarzio <lgarzio@marine.rutgers.edu>
2018-10-04 12:23:14 -0400

    added option to plot specific time range

--- plotting/tools/plot_timeseries_deployment.py
+++ plotting/tools/plot_timeseries_deployment.py
@@ -10,22 +10,29 @@ deployment and delivery method: 1) plot all data, 2) plot data, omitting outlier
 import os
 import pandas as pd
 import xarray as xr
+import datetime as dt
 import functions.common as cf
 import functions.plotting as pf
 
 
-def main(sDir, f):
+def main(sDir, f, start_time, end_time):
     ff = pd.read_csv(os.path.join(sDir, f))
     datasets = cf.get_nc_urls(ff['outputUrl'].tolist())
     for d in datasets:
         print d
-        fname, subsite, refdes, method, stream, deployment = cf.nc_attributes(d)
-        save_dir = os.path.join(sDir, subsite, refdes, deployment)
-        cf.create_dir(save_dir)
-
         with xr.open_dataset(d, mask_and_scale=False) as ds:
             ds = ds.swap_dims({'obs': 'time'})
             sci_vars = pf.science_vars(ds.data_vars.keys())
+
+            if start_time is not None and end_time is not None:
+                ds = ds.sel(time=slice(start_time, end_time))
+                if len(ds['time'].data) == 0:
+                    print 'No data to plot from specified time range'
+                    continue
+
+            fname, subsite, refdes, method, stream, deployment = cf.nc_attributes(d)
+            save_dir = os.path.join(sDir, subsite, refdes, deployment)
+            cf.create_dir(save_dir)
 
             t = ds['time'].data
             t0 = pd.to_datetime(t.min()).strftime('%Y-%m-%dT%H:%M:%S')
@@ -51,4 +58,6 @@ def main(sDir, f):
 if __name__ == '__main__':
     sDir = '/Users/lgarzio/Documents/OOI/DataReviews'
     f = 'data_request_summary.csv'
-    main(sDir, f)
+    start_time = None  # dt.datetime(2015, 4, 20, 0, 0, 0)  # optional, set to None if plotting all data
+    end_time = None  # dt.datetime(2017, 5, 20, 0, 0, 0)  # optional, set to None if plotting all data
+    main(sDir, f, start_time, end_time)
--- plotting/tools/plot_timeseries_panel.py
+++ plotting/tools/plot_timeseries_panel.py
@@ -13,11 +13,12 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+import datetime as dt
 import functions.common as cf
 import functions.plotting as pf
 
 
-def main(sDir, f):
+def main(sDir, f, start_time, end_time):
     ff = pd.read_csv(os.path.join(sDir, f))
     url_list = ff['outputUrl'].tolist()
     for u in url_list:
@@ -27,16 +28,23 @@ def main(sDir, f):
 
         for d in datasets_sel:
             print d
-            fname, subsite, refdes, method, stream, deployment = cf.nc_attributes(d)
-            save_dir = os.path.join(sDir, subsite, refdes)
-            cf.create_dir(save_dir)
-
-            sci_vars = cf.return_science_vars(stream)
-
-            colors = cm.jet(np.linspace(0, 1, len(sci_vars)))
-
             with xr.open_dataset(d, mask_and_scale=False) as ds:
                 ds = ds.swap_dims({'obs': 'time'})
+
+                if start_time is not None and end_time is not None:
+                    ds = ds.sel(time=slice(start_time, end_time))
+                    if len(ds['time'].data) == 0:
+                        print 'No data to plot from specified time range'
+                        continue
+
+                fname, subsite, refdes, method, stream, deployment = cf.nc_attributes(d)
+                save_dir = os.path.join(sDir, subsite, refdes)
+                cf.create_dir(save_dir)
+
+                sci_vars = cf.return_science_vars(stream)
+
+                colors = cm.jet(np.linspace(0, 1, len(sci_vars)))
+
                 t = ds['time'].data
                 t0 = pd.to_datetime(t.min()).strftime('%Y-%m-%dT%H:%M:%S')
                 t1 = pd.to_datetime(t.max()).strftime('%Y-%m-%dT%H:%M:%S')
@@ -53,4 +61,6 @@ def main(sDir, f):
 if __name__ == '__main__':
     sDir = '/Users/lgarzio/Documents/OOI/DataReviews'
     f = 'data_request_summary_copy.csv'
-    main(sDir, f)
+    start_time = dt.datetime(2015, 4, 20, 0, 0, 0)  # optional, set to None if plotting all data
+    end_time = dt.datetime(2016, 5, 20, 0, 0, 0)  # optional, set to None if plotting all data
+    main(sDir, f, start_time, end_time)
