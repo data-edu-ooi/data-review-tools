4b941346d6b45fa9a32e205b29e277352791a146
lgarzio <lgarzio@marine.rutgers.edu>
2018-12-18 08:44:26 -0500

    update __init__.py

--- plotting/scripts/__init__.py
+++ plotting/scripts/__init__.py
@@ -3,4 +3,5 @@ from . import plot_profiles
 from . import plot_timeseries
 from . import plot_timeseries_all
 from . import plot_timeseries_panel
+from . import plot_timeseries_pm_all
 from . import plot_xsection
