f048409373ca54629efb9de6182009c4bcb3afa1
lgarzio <lgarzio@marine.rutgers.edu>
2018-12-18 14:50:24 -0500

    final stats

---
+++ data_review/run_final_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Created on Dec 18 2018 by Lori Garzio
+@brief This is a wrapper script that imports tools to calculate final statistics for science variables in multiple datasets.
+@usage
+sDir: location to save summary output
+f: file containing THREDDs urls with .nc files to analyze. The column containing the THREDDs urls must be labeled
+'outputUrl' (e.g. an output from one of the data_download scripts)
+"""
+
+import pandas as pd
+import scripts
+
+sDir = '/Users/lgarzio/Documents/repo/OOI/data-edu-ooi/data-review-tools/data_review/final_stats'
+f = '/Users/lgarzio/Documents/OOI/DataReviews/GI/GI03FLMA/data_request_summary_run1.csv'
+
+ff = pd.read_csv(f)
+url_list = ff['outputUrl'].tolist()
+url_list = [u for u in url_list if u not in 'no_output_url']
+
+scripts.final_ds_stats.main(sDir, url_list)
